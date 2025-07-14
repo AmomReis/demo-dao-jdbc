f737a8b2bc4695698850383cbb2468d1f7d1d05d
Amom Reis <mirandoamom@gmail.com>
2025-07-14 20:31:26 -0300

    including comments in DaoFactory

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -4,12 +4,26 @@ import db.DB;
 import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
+/**
+ * Factory class responsible for instantiating DAO implementations.
+ * This promotes loose coupling by abstracting the concrete DAO classes from the application code.
+ */
 public class DaoFactory {
 
+    /**
+     * Creates an instance of SellerDao using a JDBC implementation.
+     *
+     * @return a new instance of SellerDaoJDBC with an active database connection
+     */
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
     }
 
+    /**
+     * Creates an instance of DepartmentDao using a JDBC implementation.
+     *
+     * @return a new instance of DepartmentDaoJDBC with an active database connection
+     */
     public static DepartmentDao createDepartmentDao() {
         return new DepartmentDaoJDBC(DB.getConnection());
     }
